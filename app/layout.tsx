04b8e3b21d41b7e7aac7f19ee8687d6b46faaad2
Uriel Sabugo <urielmaximiliano27@icloud.com>
2025-06-04 17:09:04 -0300

    final

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'LA ROCKA SHOP',
+  description: 'Created with love by La Rocka Shop',
+  generator: 'La Rocka Shop',
 }
 
 export default function RootLayout({
